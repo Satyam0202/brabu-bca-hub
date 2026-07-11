fa6949f8b165c18f6729af99031e09d310cf81f2
Satyam0202 <m4740600@gmail.com>
2026-07-11 14:11:38 +0530

    Match Semester 1 session label to local folder

--- src/utils/content.js
+++ src/utils/content.js
@@ -6,7 +6,7 @@ import semester5 from "../data/semester5.json";
 import semester6 from "../data/semester6.json";
 
 export const semesters = [
-  { number: 1, title: "Semester 1", subjects: semester1, sessions: ["2024-27", "2023-26", "2022-25", "2021-24", "2020-23", "2019-22"] },
+  { number: 1, title: "Semester 1", subjects: semester1, sessions: ["2024-27", "2023-24", "2022-25", "2021-24", "2020-23", "2019-22"] },
   { number: 2, title: "Semester 2", subjects: semester2, sessions: ["2024-27", "2023-26", "2022-25", "2021-24", "2020-23", "2019-22"] },
   { number: 3, title: "Semester 3", subjects: semester3, sessions: ["2023-26", "2022-25", "2021-24", "2020-23", "2019-22"] },
   { number: 4, title: "Semester 4", subjects: semester4, sessions: ["2023-26", "2022-25", "2021-24", "2020-23", "2019-22"] },
